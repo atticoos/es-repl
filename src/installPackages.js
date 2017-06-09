943b2300cf7c40ea36f7faa0df60260f2278b9a6
Atticus White <contact@atticuswhite.com>
2017-06-09 00:56:09 -0400

    installation spinner and smarter module detection

--- src/installPackages.js
+++ src/installPackages.js
@@ -1,13 +1,23 @@
 const fs = require('fs')
 const path = require('path')
+const Ora = require('ora')
+const chalk = require('chalk')
 const npm = require('npm-programmatic')
 const getReplPath = require('./getReplPath')
 
+
 module.exports = function installPackages (packages) {
+  const spinner = new Ora()
+  spinner.text = `Installing ${packages.length} packages`
+  spinner.start()
   return npm.install(packages, {
     cwd: getReplPath(),
-    output: true,
     save: false,
     noSave: true
+  }).then(() => {
+    spinner.succeed(chalk.green(`Installed ${packages.length} packages`))
+  }).catch(e => {
+    spinner.fail(chalk.red('Installation failed'))
+    throw e
   })
 }
--- src/moduleExists.js
+++ src/moduleExists.js
@@ -1,8 +1,21 @@
+const path = require('path')
+const getReplPath = require('./getReplPath')
+
 module.exports = function moduleExists (moduleName) {
   try {
     require.resolve(moduleName);
     return true;
   } catch (e) {
-    return false;
+    try {
+      require.resolve(
+        path.join(
+          getReplPath(),
+          'node_modules',
+          moduleName
+        )
+      )
+      return true
+    } catch (e) { }
   }
+  return false;
 }
